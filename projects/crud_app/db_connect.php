4f0e7ddba09613b65309bd9da110e1cad9cd51d9
Cleyton <lamunerd@gmail.com>
2025-05-16 19:51:25 -0300

    adiciona arquivos do portfolio

---
+++ projects/crud_app/db_connect.php
@@ -0,0 +1,16 @@
+<?php 
+
+$host = 'localhost';
+$dbname = 'crud_app';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}   catch (PDOException $e) {
+    echo "Erro de conexão: " . $e->getMessage();
+    exit();
+}
+
+?>
